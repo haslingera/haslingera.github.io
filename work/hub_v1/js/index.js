bd6858f13f745fcde41c61528709d8209c27021c
Andreas Haslinger <andreas.haslinger@dynatrace.com>
2020-09-15 17:34:40 +0200

    created a first search version

---
+++ work/hub_v1/js/index.js
@@ -0,0 +1,72 @@
+var increment = 0.05;
+var hideTimer;
+var showTimer;
+
+var menuicons = document.querySelectorAll('.animate');
+for (var i = 0; i < menuicons.length; i++) {
+    menuicons.item(i).style.cssText = `--delay1: ${0.1 + (i * increment)}s; --delay2: ${0.15 + (i * increment)}s;`;
+}
+
+document.querySelector('.header-container').addEventListener('mouseenter', enter)
+document.querySelector('.header-container').addEventListener('mouseleave', leave)
+
+function leave(event) {
+    hideTimer = setTimeout(() => {
+        event.target.classList.remove("show-dock");
+    }, 1000)
+    clearTimeout(showTimer);
+}
+
+function enter(event) {
+    showTimer = setTimeout(() => {
+        event.target.classList.add("show-dock");
+    }, 200)
+    clearTimeout(hideTimer);
+}
+
+let recents = document.querySelector('.recents');
+let filters = document.querySelector('.filters');
+let search = document.querySelector('.search');
+let searchHeader = document.querySelector('.search-results-header');
+let searchIcon = document.querySelector('.search-icon');
+let input = document.querySelector('.search-input');
+let welcomeMessage = document.querySelector('.welcome-message');
+
+input.addEventListener('input', e => {
+    checkInput();
+})
+
+searchIcon.addEventListener('click', e => {
+    input.value = '';
+    checkInput();
+})
+
+document.onkeypress= function (e){
+    input.focus();
+}
+
+function checkInput () {
+    let value = input.value;
+    let randomSearchResult = Math.floor(Math.random() * 10000);
+
+    if (value == '') {
+        recents.classList.remove('hidden');
+        filters.classList.remove('hidden');
+        search.classList.add('hidden');
+        searchIcon.classList.add('invisible');
+        welcomeMessage.classList.remove('invisible');
+    } else {
+        recents.classList.add('hidden');
+        filters.classList.add('hidden');
+        search.classList.remove('hidden');
+        searchIcon.classList.remove('invisible');
+        welcomeMessage.classList.add('invisible');
+        searchHeader.innerHTML = `${randomSearchResult.toLocaleString()} search results for '${value.trim()}'`        
+    }
+}
+
+function fillInput (e) {
+    input.value = e.innerHTML;
+    checkInput ();
+}
+
